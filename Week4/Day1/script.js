70f196b14d53eb017f513cd2c81066cf941a11ac
Rubin <smrubin@labs.neit.edu>
2021-04-21 12:42:52 -0400

    Created first Canvas Lab

---
+++ Week4/Day1/script.js
@@ -0,0 +1,31 @@
+//Defines Variable to allow canvas access.
+var canvas = document.getElementById('canvas1');
+//Define Drawing Context of canvas element
+var ctx = canvas.getContext('2d');
+
+//Draw Square/Rectangle
+ctx.fillStyle = "cyan";
+ctx.strokeStyle = "black";
+ctx.lineWidth = "3"
+ctx.fillRect(30, 30, 100, 100);
+ctx.strokeRect(30, 30, 100, 100);
+
+//Draw Line
+ctx.strokeStyle = "yellow"
+ctx.moveTo(0,0);
+ctx.lineTo(800,600);
+ctx.stroke();
+
+ctx.moveTo(800,0);
+ctx.lineTo(0,600);
+ctx.stroke();
+
+//Draw Circle
+
+ctx.beginPath();
+// ctx.Arc(Pos X, Pos Y, Radius, Start Angle, Radian (Radian Cicles 3pi/2 is 75 degrees), isCounterClockwise(True or false).
+ctx.arc(400, 300, 50, 0, (3 * Math.PI)/2, false);
+//Adding a point before it finishes the path to the center of the screen, IE Makes it got to 400, 300 before closing the circle.
+ctx.lineTo(400,300);
+ctx.closePath();
+ctx.fill();
